6b66d13ab2911f9b3bd25467c5bf2f905a6d578e
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-02-07 10:17:45 +0100

    add ESLint and husky

--- .history/src/components/Card/Card_20200206230128.js
+++ .history/src/components/Card/Card_20200206230128.js
@@ -5,15 +5,15 @@ import PropTypes from 'prop-types';
 class Card extends React.Component {
 
     static PropTypes = {
-      title: PropTypes.node.isRequired,
+        title: PropTypes.node.isRequired,
     }
 
     render () {
-      return (
-        <div className= {styles.component}>
-          <h3 className= {styles.title}> {this.props.title}</h3>
-        </div>
-      );
+        return (
+            <div className= {styles.component}>
+                <h3 className= {styles.title}> {this.props.title}</h3>
+            </div>
+        )
     }
 }
 
