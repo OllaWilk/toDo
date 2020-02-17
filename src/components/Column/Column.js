333715adb18f393ceaabd7842c2c12153ea52e73
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-02-17 13:43:04 +0100

    16.4.b mapp dispatches to props

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,25 +2,26 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
-//import Creator from '../Creator/Creator';
+import Creator from '../Creator/Creator';
 import Icon from '../Icon/Icon';
 import {settings} from '../../data/dataStore';
 
 class Column extends React.Component {
 
+    static propTypes= {
+      title: PropTypes.node.isRequired,
+      cards: PropTypes.array,
+      icon: PropTypes.node,
+      addCard: PropTypes.func,
+    }
+
     static defaultProps = {
       icon: settings.defaultColumnIcon,
     }
 
-    static propTypes= {
-      title: PropTypes.node.isRequired,
-      cards: PropTypes.array,
-      icon: PropTypes.string,
-    }
-
     render() {
 
-      const {title, icon, cards} = this.props;
+      const {title, icon, cards, addCard} = this.props;
 
       return (
         <section className= {styles.component}>
@@ -35,9 +36,9 @@ class Column extends React.Component {
               <Card key={cardData.id} {...cardData} />
             ))}
           </div>
-          {/*<div className= {styles.creator}>
-            <Creator text= {settings.cardCreatorText} action= {title => this.addCard(title)} />
-            </div>*/}
+          <div className= {styles.creator}>
+            <Creator text= {settings.cardCreatorText} action= {addCard} />
+          </div>
         </section>
       );
     }
