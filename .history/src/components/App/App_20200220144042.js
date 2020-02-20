6301fccb97d4351d24a8ae90f73c9245d28f7a4a
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-02-20 21:22:40 +0100

    add animation

--- .history/src/components/App/App_20200220144042.js
+++ .history/src/components/App/App_20200220144042.js
@@ -4,22 +4,15 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Info from '../Info/Info';
 import MainLayout from '../MainLayout/MainLayout';
 import Faq from '../Faq/Faq';
-import {AnimatedSwitch} from 'react-router-transition';
-
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <AnimatedSwitch
-        atEnter={{ opacity: 0 }}
-        atLeave={{ opacity: 0 }}
-        atActive={{ opacity: 1 }}
-        className="switch-wrapper"
-      >
+      <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/faq' component={Faq} />
-      </AnimatedSwitch>
+      </Switch>
     </MainLayout>
   </BrowserRouter>
 );
--- .history/src/components/App/App_20200220211646.js
+++ .history/src/components/App/App_20200220211646.js
@@ -10,16 +10,11 @@ import {AnimatedSwitch} from 'react-router-transition';
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <AnimatedSwitch
-        atEnter={{ opacity: 0 }}
-        atLeave={{ opacity: 0 }}
-        atActive={{ opacity: 1 }}
-        className="switch-wrapper"
-      >
+      <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/faq' component={Faq} />
-      </AnimatedSwitch>
+      </Switch>
     </MainLayout>
   </BrowserRouter>
 );
